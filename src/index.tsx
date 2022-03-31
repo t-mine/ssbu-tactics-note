027b664df53b9966d1f4e4af7384525e303059f7
Tomonori Mine <tomonori.mine.biz@gmail.com>
2022-03-31 22:53:41 +0900

    #10 Cognitoでとりあえずログインできるようにした

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,30 @@ import { ChakraProvider, Stack } from '@chakra-ui/react';
 import TestPage from './pages/TestPage';
 import Header from './components/Header';
 
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsconfig from '../src/aws-exports';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure(awsconfig);
+
 function App() {
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Stack>
-          <Header></Header>
-          <Routes>
-            <Route path="/" element={<Navigate to="/test" />} />
-            <Route path="/test" element={<TestPage />} />
-          </Routes>
-        </Stack>
-      </BrowserRouter>
-    </ChakraProvider>
+    <Authenticator>
+      {() => (
+        <ChakraProvider>
+          <BrowserRouter>
+            <Stack>
+              <Header></Header>
+              <Routes>
+                <Route path="/" element={<Navigate to="/test" />} />
+                <Route path="/test" element={<TestPage />} />
+              </Routes>
+            </Stack>
+          </BrowserRouter>
+        </ChakraProvider>
+      )}
+    </Authenticator>
   );
 }
 
