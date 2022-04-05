8dc3cef78bf42b3788f18b4d0eb4dcbef6a9d6ad
Tomonori Mine <tomonori.mine.biz@gmail.com>
2022-04-06 00:45:30 +0900

    #10 ログイン機能を仮実装

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -13,12 +13,32 @@ import {
   useDisclosure,
   Link,
 } from '@chakra-ui/react';
-import React, { RefObject } from 'react';
+import { Auth } from 'aws-amplify';
+import React, { RefObject, useState } from 'react';
 
 const SignInButton = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const initialRef: RefObject<any> = React.useRef();
+
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  };
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onSignInButtonClick = async () => {
+    try {
+      const user = await Auth.signIn(id, password);
+      console.log(JSON.stringify(user));
+      // TODO userの中身をjotaiに格納するしかない？
+    } catch (error) {
+      console.log('error signing in', error);
+    }
+  };
 
   return (
     <>
@@ -27,25 +47,28 @@ const SignInButton = () => {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create your account</ModalHeader>
+          {/* Header */}
+          <ModalHeader></ModalHeader>
+          {/* Close Button */}
           <ModalCloseButton />
+          {/* Body */}
           <ModalBody pb={6}>
+            {/* ID */}
             <FormControl>
-              <FormLabel>First name</FormLabel>
-              <Input ref={initialRef} placeholder="First name" />
+              <FormLabel>ID</FormLabel>
+              <Input ref={initialRef} placeholder="ID" onChange={handleIdChange} />
             </FormControl>
-
+            {/* Password */}
             <FormControl mt={4}>
-              <FormLabel>Last name</FormLabel>
-              <Input placeholder="Last name" />
+              <FormLabel>パスワード</FormLabel>
+              <Input type="password" placeholder="パスワード" onChange={handlePasswordChange} />
             </FormControl>
           </ModalBody>
-
+          {/* Footer */}
           <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
-              Save
+            <Button onClick={onSignInButtonClick} colorScheme="blue" mr={3}>
+              ログイン
             </Button>
-            <Button onClick={onClose}>Cancel</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
