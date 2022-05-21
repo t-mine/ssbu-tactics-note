0d3052d364648069c4f10337ad1568cf77ca92b9
t-mine <tomonori.mine.biz@gmail.com>
2022-05-22 04:54:16 +0900

    PrivateRouteをシンプルに修正

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,17 +1,10 @@
 import { useAtom } from 'jotai';
-import { Fragment, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 import { loginInfoAtom } from '../atom/atom';
 
-const PrivateRoute: React.FC = (props: any) => {
+const PrivateRoute: React.FC = ({ children }) => {
   const [loginInfo] = useAtom(loginInfoAtom);
-
-  useEffect(() => {
-    if (!loginInfo.username) {
-      window.location.href = '/';
-    }
-  }, []);
-
-  return <Fragment>{props.children}</Fragment>;
+  return loginInfo.username ? <>{children}</> : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
